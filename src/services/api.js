ac1fb9f338b068256ac9ece19cb499b1404d7f2d
miguelszg <ms462974@gmail.com>
2025-03-30 09:41:46 -0600

    Cambio api 3

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL base de tu API
-const API_URL = 'https://server-f1.vercel.app';
+const API_URL = 'https://server-f1.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
